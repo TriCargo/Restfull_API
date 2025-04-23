41e691e101d8f0097fd7d608572306b6ae4d00ca
TriCargo <phunghoanglua2005@gmail.com>
2025-04-23 23:02:08 +0700

    Delete RestfullAPI directory

--- RestfullAPI/src/main/java/com/student/dao/UssersDAO.java
+++
@@ -1,8 +0,0 @@
-package com.student.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.student.entity.Users;
-
-public interface UssersDAO extends JpaRepository<Users, String> {
-}
